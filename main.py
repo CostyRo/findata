91c83a0aa4f7a2754771d6fa400477623b692379
CostyRo <ioncotinosu12@gmail.com>
2022-02-22 17:37:32 +0200

    stock command

--- main.py
+++ main.py
@@ -5,14 +5,29 @@ from click import argument,command,echo,group,option,secho
 
 @group()
 def cli():
-    """"""
+  """"""
 
 @cli.command()
 @argument("ticker")
-def mcap(ticker):
-  ticker_object=Ticker(ticker)
+@option("-c","--crypto","crypto",is_flag=True)
+@option("-f","--format","_format",is_flag=True)
+def mcap(ticker,crypto,_format):
+  ticker_object=Ticker(f"{ticker}-usd") if crypto else Ticker(ticker)
   ticker_info=ticker_object.info
-  if ticker_info["longName"] is not None: echo(f"""Market cap of {ticker_info["longName"]}({ticker_object.ticker}) is {ticker_info["marketCap"]}$""")
-  else: secho(f"""Ticker: "{ticker}" doesn't exists!""",fg="red")
+  try:
+    market_cap=f"""{ticker_info["marketCap"]:,}""".replace(","," ") if _format else ticker_info["marketCap"]
+    echo(f"""Market cap of {ticker.upper()} is {market_cap}$""")
+  except KeyError: secho(f"""Ticker: "{ticker.upper()}" doesn't exists!""",fg="red")
+
+@cli.command()
+@argument("ticker")
+@option("-c","--crypto","crypto",is_flag=True)
+def stock(ticker,crypto):
+  ticker_object=Ticker(f"{ticker}-usd") if crypto else Ticker(ticker)
+  ticker_info=ticker_object.info
+  try:
+    price=ticker_info["regularMarketPrice"] if crypto else ticker_info["currentPrice"]
+    echo(f"""{"Value" if crypto else "Stock"} of {ticker.upper()} is {price}$""")
+  except KeyError: secho(f"""Ticker: "{ticker.upper()}" doesn't exists!""",fg="red")
 
 if __name__=="__main__": cli()
