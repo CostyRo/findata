1b91c0c32af38cc7c02e6799f6a46bc02e3889fc
CostyRo <ioncotinosu12@gmail.com>
2022-02-22 14:23:02 +0200

    mcap command

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+from yfinance import Ticker
+from click import argument,command,echo,group,option,secho
+
+@group()
+def cli():
+    """"""
+
+@cli.command()
+@argument("ticker")
+def mcap(ticker):
+  ticker_object=Ticker(ticker)
+  ticker_info=ticker_object.info
+  if ticker_info["longName"] is not None: echo(f"""Market cap of {ticker_info["longName"]}({ticker_object.ticker}) is {ticker_info["marketCap"]}$""")
+  else: secho(f"""Ticker: "{ticker}" doesn't exists!""",fg="red")
+
+if __name__=="__main__": cli()
