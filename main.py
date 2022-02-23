d6bb364c1b72640db241b5669479e6075011e382
CostyRo <ioncotinosu12@gmail.com>
2022-02-23 14:59:37 +0200

    fixing problems

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+from matplotlib import rcParams
 
 from yfinance import Ticker
 from pandas import set_option
 import matplotlib.pyplot as plt
-from click import argument,command,echo,group,launch,option,Path,secho
+from matplotlib import rcParams
+from click import argument,echo,group,launch,option,Path,secho
 
 @group()
 def cli():
@@ -11,7 +13,7 @@ def cli():
 
 @cli.command()
 @argument("ticker")
-@option("-n","-noprint","no_print",is_flag=True)
+@option("-n","--noprint","no_print",is_flag=True)
 @option("-p","--plot","plot",is_flag=True)
 @option("-s","--saveplot","save_plot",type=Path())
 @option("-c","--csv","csv",type=Path())
@@ -22,31 +24,38 @@ def earnings(ticker,no_print,save_plot,plot,csv,excel):
     if not no_print:
         echo(f"{ticker.upper()} Earnings")
         echo(ticker_earnings)
-    if plot or save_plot: ticker_earnings.plot(title=f"{ticker.upper()} Earnings",kind="bar")
+    if plot or save_plot: ticker_earnings.plot(title=f"{ticker.upper()} Earnings",kind="bar",logy=True,rot=0)
     if plot: plt.show()
-    if save_plot: plt.savefig(save_plot)
+    if save_plot: plt.savefig(f"{save_plot}.svg",format="svg",dpi=1200)
     if csv: ticker_earnings.to_csv(csv)
-    if excel: ticker_earnings.to_excel(excel)
+    if excel:
+      excel=excel if excel.endswith(".xlsx") else excel+".xlsx"
+      ticker_earnings.to_excel(excel)
   else: secho(f"""Ticker: "{ticker.upper()}" doesn't exists or doesn't have earnings!""",fg="red")
 
 @cli.command()
 @argument("ticker")
-@option("-n","-noprint","no_print",is_flag=True)
+@option("-y","--crypto","crypto",is_flag=True)
+@option("-n","--noprint","no_print",is_flag=True)
 @option("-p","--plot","plot",is_flag=True)
 @option("-s","--saveplot","save_plot",type=Path())
 @option("-c","--csv","csv",type=Path())
 @option("-x","--excel","excel",type=Path())
-def history(ticker,no_print,save_plot,plot,csv,excel):
-  ticker_history=Ticker(ticker).history()
+def history(ticker,crypto,no_print,save_plot,plot,csv,excel):
+  ticker_history=Ticker(f"{ticker}-usd").history() if crypto else Ticker(ticker).history()
   if not ticker_history.empty:
     if not no_print:
         echo(f"{ticker.upper()} History")
         echo(ticker_history)
-    if plot or save_plot: ticker_history.Volume.plot(title=f"{ticker.upper()} Stock Volume",kind="bar")
+    if plot or save_plot:
+      ticker_history.index=[str(i)[:10] for i in ticker_history.index]
+      ticker_history.Volume.plot(title=f"{ticker.upper()} Stock Volume",kind="bar",logy=True)
     if plot: plt.show()
-    if save_plot: plt.savefig(save_plot)
+    if save_plot: plt.savefig(f"{save_plot}.svg",format="svg",dpi=1200)
     if csv: ticker_history.to_csv(csv)
-    if excel: ticker_history.to_excel(excel)
+    if excel:
+      excel=excel if excel.endswith(".xlsx") else excel+".xlsx"
+      ticker_history.to_excel(excel)
   else: secho(f"""Ticker: "{ticker.upper()}" doesn't exists or doesn't have history!""",fg="red")
 
 @cli.command()
@@ -82,7 +91,7 @@ def stock(ticker,crypto):
 
 @cli.command()
 @argument("ticker")
-@option("-n","-noprint","no_print",is_flag=True)
+@option("-n","--noprint","no_print",is_flag=True)
 @option("-p","--plot","plot",is_flag=True)
 @option("-s","--saveplot","save_plot",type=Path())
 @option("-c","--csv","csv",type=Path())
@@ -93,16 +102,18 @@ def qearnings(ticker,no_print,save_plot,plot,csv,excel):
     if not no_print:
         echo(f"{ticker.upper()} Quarterly Earnings")
         echo(ticker_qearnings)
-    if plot or save_plot: ticker_qearnings.plot(title=f"{ticker.upper()} Quarterly Earnings",kind="bar")
+    if plot or save_plot: ticker_qearnings.plot(title=f"{ticker.upper()} Quarterly Earnings",kind="bar",logy=True,rot=0)
     if plot: plt.show()
-    if save_plot: plt.savefig(save_plot)
+    if save_plot: plt.savefig(f"{save_plot}.svg",format="svg",dpi=1200)
     if csv: ticker_qearnings.to_csv(csv)
-    if excel: ticker_qearnings.to_excel(excel)
+    if excel:
+      excel=excel if excel.endswith(".xlsx") else excel+".xlsx"
+      ticker_qearnings.to_excel(excel)
   else: secho(f"""Ticker: "{ticker.upper()}" doesn't exists or doesn't have quarterly earnings!""",fg="red")
 
 @cli.command()
 @argument("ticker")
-@option("-n","-noprint","no_print",is_flag=True)
+@option("-n","--noprint","no_print",is_flag=True)
 @option("-c","--csv","csv",type=Path())
 @option("-x","--excel","excel",type=Path())
 def recommendations(ticker,no_print,csv,excel):
@@ -112,10 +123,12 @@ def recommendations(ticker,no_print,csv,excel):
         echo(f"{ticker.upper()} Recommendations")
         echo(ticker_recommendations)
     if csv: ticker_recommendations.to_csv(csv)
-    if excel: ticker_recommendations.to_excel(excel)
+    if excel:
+      excel=excel if excel.endswith(".xlsx") else excel+".xlsx"
+      ticker_recommendations.to_excel(excel)
   else: secho(f"""Ticker: "{ticker.upper()}" doesn't exists or doesn't have recommendations!""",fg="red")
 
 if __name__=="__main__":
   set_option("display.max_rows",None)
-  plt.ticklabel_format(style="plain")
+  rcParams.update({"figure.autolayout": True})
   cli()
