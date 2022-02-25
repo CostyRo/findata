ffa92ddf48e1ddb368d7c4e2b0ce9ae10509918d
CostyRo <ioncotinosu12@gmail.com>
2022-02-25 22:43:29 +0200

    finishing commenting

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from click import argument,echo,group,launch,option,Path,secho
 def cli():
   """"""
 
-# create command for custom the options of the app
+# create the command for custom the options of the app
 @cli.command()
 @argument("option",required=False)
 @argument("new_value",required=False)
@@ -28,12 +28,12 @@ def custom(option=None,new_value=None):
 
   if option is None:
     for key,value in settings.items(): echo(f"""Setting "{key}" have the value of "{value}".""")
-    # if no argument is given loop the settings and print the setting and value of it
-  elif new_value is None: echo(f"""Setting "{option}" "{settings[option]}".""")
-  # if a setting is given print the value of the given setting
+    # if no argument is given, loop the settings and print the setting and the value of it
+  elif new_value is None: echo(f"""Setting "{option}" have value of "{settings[option]}".""")
+  # if a setting is given, print the value of the given setting
   else:
     settings[option]=new_value
-    # if a new value is given change the value of the option
+    # if a new value is given, change the value of the option
 
     with open("settings.json","w") as f: dump_json(settings,f,indent=4)
     # update the settings
@@ -41,7 +41,7 @@ def custom(option=None,new_value=None):
     echo(f"""Setting "{option} was changed to "{new_value}".""")
     # announce that the value was changed
 
-# create command for getting the earnings of a ticker
+# create the command for getting the earnings of a ticker
 @cli.command()
 @argument("ticker")
 @option("-n","--noprint","no_print",is_flag=True)
@@ -49,14 +49,14 @@ def custom(option=None,new_value=None):
 @option("-s","--saveplot","save_plot",type=Path())
 @option("-c","--csv","csv",type=Path())
 @option("-x","--excel","excel",type=Path())
-def earnings(ticker,no_print,save_plot,plot,csv,excel):
+def earnings(ticker,no_print,plot,save_plot,csv,excel):
 
   """"""
 
   # verify if earnings exists
   if not ((ticker_earnings:=Ticker(ticker).earnings).empty):
     if not no_print: echo(f"{ticker.upper()} Earnings\n{ticker_earnings}")
-    # print the earnings if the user didn't disabled the printing
+    # if the user didn't disable the printing, print the earnings
 
     if plot or save_plot: ticker_earnings.plot(title=f"{ticker.upper()} Earnings",kind="bar",logy=True,rot=0)
     # if the user wants plotting, load the plot
@@ -65,16 +65,17 @@ def earnings(ticker,no_print,save_plot,plot,csv,excel):
     # if the user wants, show the plot
 
     if save_plot: plt.savefig(f"{save_plot}.svg",format="svg",dpi=1200)
+    # if the user wants, save the plot as a svg file
 
     if csv: ticker_earnings.to_csv(csv,settings["csv_separator"])
-    # save into a csv earnings if the user want this
+    # if the user wants, save earnings into a csv
 
     if excel: ticker_earnings.to_excel(excel if excel.endswith(".xlsx") else excel+".xlsx")
-    # save into a excel earnings if the user want this
+    # if the user wants, save earnings into a excel
   else: secho(f"""Ticker: "{ticker.upper()}" doesn't exists or doesn't have earnings!""",fg="red")
   # otherwise show an error that earnings didn't exists
 
-# create command for getting the history of a ticker
+# create the command for getting the history of a ticker
 @cli.command()
 @argument("ticker")
 @option("-y","--crypto","crypto",is_flag=True)
@@ -83,36 +84,41 @@ def earnings(ticker,no_print,save_plot,plot,csv,excel):
 @option("-s","--saveplot","save_plot",type=Path())
 @option("-c","--csv","csv",type=Path())
 @option("-x","--excel","excel",type=Path())
-def history(ticker,crypto,no_print,save_plot,plot,csv,excel):
+def history(ticker,crypto,no_print,plot,save_plot,csv,excel):
 
   """"""
 
-  ticker_object=Ticker(f"{ticker}-usd") if crypto else Ticker(ticker)
+  ticker_history=(Ticker(f"{ticker}-usd") if crypto else Ticker(ticker)).history()
+  # save the history about the ticker in a dictionary
 
   # verify if history exists
-  if not ((ticker_history:=ticker_object.history()).empty):
+  if not ticker_history.empty:
     if not no_print: echo(f"{ticker.upper()} History\n{ticker_history}")
-    # print the history if the user didn't disabled the printing
+    # if the user didn't disable the printing, print the history
 
+    # if the user wants plotting, load the plot
     if plot or save_plot:
       ticker_history.index=[str(i)[:10] for i in ticker_history.index]
+      # update the index column with the shorter version of the time
 
       ticker_history.Volume.plot(title=f"{ticker.upper()} Stock Volume",kind="bar",logy=True)
-  
+      # load only the column with volume as a plot
+
     if plot: plt.show()
     # if the user wants, show the plot
 
     if save_plot: plt.savefig(f"{save_plot}.svg",format="svg",dpi=1200)
+    # if the user wants, save the plot as a svg file
 
     if csv: ticker_history.to_csv(csv,settings["csv_separator"])
-    # save into a csv history if the user want this
+    # if the user wants, save history into a csv
 
     if excel: ticker_history.to_excel(excel if excel.endswith(".xlsx") else excel+".xlsx")
-    # save into a excel history if the user want this
+    # if the user wants, save history into a excel
   else: secho(f"""Ticker: "{ticker.upper()}" doesn't exists or doesn't have history!""",fg="red")
   # otherwise show an error that history didn't exists
 
-# create command for getting the market cap of a ticker
+# create the command for getting the market cap of a ticker
 @cli.command()
 @argument("ticker")
 @option("-y","--crypto","crypto",is_flag=True)
@@ -129,7 +135,7 @@ def mcap(ticker,crypto):
   # try to acces the market cap and print the value
   # if a error is occurred, announce the user that the ticker doesn't exists
 
-# create command for getting the news of a ticker
+# create the command for getting the news of a ticker
 @cli.command()
 @argument("ticker")
 @option("-o","--open","_open",is_flag=True)
@@ -144,7 +150,7 @@ def news(ticker,_open):
     if _open: launch(item["link"])
     # if user want open the news in the browser
 
-# create command for getting the stock/price of a ticker
+# create the command for getting the stock/price of a ticker
 @cli.command()
 @argument("ticker")
 @option("-y","--crypto","crypto",is_flag=True)
@@ -161,7 +167,7 @@ def stock(ticker,crypto):
   # try to acces the price and print the value
   # if a error is occurred, announce the user that the ticker doesn't exists
 
-# create command for getting the quarterly earnings of a ticker
+# create the command for getting the quarterly earnings of a ticker
 @cli.command()
 @argument("ticker")
 @option("-n","--noprint","no_print",is_flag=True)
@@ -169,14 +175,14 @@ def stock(ticker,crypto):
 @option("-s","--saveplot","save_plot",type=Path())
 @option("-c","--csv","csv",type=Path())
 @option("-x","--excel","excel",type=Path())
-def qearnings(ticker,no_print,save_plot,plot,csv,excel):
+def qearnings(ticker,no_print,plot,save_plot,csv,excel):
 
   """"""
 
   # verify if quarterly earnings exists
   if not ((ticker_qearnings:=Ticker(ticker).quarterly_earnings).empty):
     if not no_print: echo(f"{ticker.upper()} Quarterly Earnings\n{ticker_qearnings}")
-    # print the quarterly earnings if the user didn't disabled the printing
+    # if the user didn't disable the printing, print the quarterly earnings
 
     if plot or save_plot: ticker_qearnings.plot(title=f"{ticker.upper()} Quarterly Earnings",kind="bar",logy=True,rot=0)
     # if the user wants plotting, load the plot
@@ -185,16 +191,17 @@ def qearnings(ticker,no_print,save_plot,plot,csv,excel):
     # if the user wants, show the plot
 
     if save_plot: plt.savefig(f"{save_plot}.svg",format="svg",dpi=1200)
+    # if the user wants, save the plot as a svg file
 
     if csv: ticker_qearnings.to_csv(csv,settings["csv_separator"])
-    # save into a csv quarterly earnings if the user want this
+    # if the user wants, save quarterly earnings into a csv
 
     if excel: ticker_qearnings.to_excel(excel if excel.endswith(".xlsx") else excel+".xlsx")
-    # save into a excel quarterly earnings if the user want this
+    # if the user wants, save quarterly earnings into a excel
   else: secho(f"""Ticker: "{ticker.upper()}" doesn't exists or doesn't have quarterly earnings!""",fg="red")
   # otherwise show an error that quarterly earnings didn't exists
 
-# create command for getting the recommendations maded for a ticker
+# create the command for getting the recommendations maded for a ticker
 @cli.command()
 @argument("ticker")
 @option("-n","--noprint","no_print",is_flag=True)
@@ -207,17 +214,17 @@ def recommendations(ticker,no_print,csv,excel):
   # verify if recommendations exists
   if (ticker_recommendations:=Ticker(ticker).recommendations) is not None:
     if not no_print: echo(f"{ticker.upper()} Recommendations\n{ticker_recommendations}")
-    # print the recommendations if the user didn't disabled the printing
+    # print the recommendations if the user didn't disable the printing
 
     if csv: ticker_recommendations.to_csv(csv,settings["csv_separator"])
-    # save into a csv recommendations if the user want this
+    # if the user wants, save recommendations into a csv
 
     if excel: ticker_recommendations.to_excel(excel if excel.endswith(".xlsx") else excel+".xlsx")
-    # save into a excel recommendations if the user want this
+    # if the user wants, save recommendations into a excel
   else: secho(f"""Ticker: "{ticker.upper()}" doesn't exists or doesn't have recommendations!""",fg="red")
   # otherwise show an error that recommendations didn't exists
 
-# create command for getting the name of a ticker
+# create the command for getting the name of a ticker
 @cli.command()
 @argument("ticker")
 @option("-y","--crypto","crypto",is_flag=True)
@@ -233,9 +240,13 @@ def whois(ticker,crypto):
 
 if __name__=="__main__":
   set_pandas_option("display.max_rows",None)
+  # set the displayed rows to infinite
 
   rcParams.update({"figure.autolayout": True})
+  # set autolayout to true to display the ploy correctly
 
   with open("settings.json","r") as f: settings=load_json(f)
+  # load the settings in a dictionary
 
   cli()
+  # start the cli app
